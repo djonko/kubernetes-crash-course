466226d47abb3fd35f69531d517219a627117737
djonko <prevostdjonko@yahoo.fr>
2021-07-11 23:52:41 -0400

    section 5 end

--- 02-todo-web-application-h2/src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
+++ 02-todo-web-application-h2/src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com.in28minutes.springboot.web")
+@SpringBootApplication(scanBasePackages = "com.in28minutes.springboot.web")
 public class SpringBootFirstWebApplication extends SpringBootServletInitializer {
 
 	@Override
